39dbb21099a2c89c852a560ad4b9f8df79a7d6e3
Sebirobth <osuisreallyfun@gmail.com>
2022-12-04 01:07:52 -0500

    Changed guildMemberAdd event

--- Events/guild/guildMemberAdd.js
+++ Events/guild/guildMemberAdd.js
@@ -1,7 +1,5 @@
+const Config = require('../../config.json');
+
 module.exports = (Discord, client, member) => {
-
-    const welcChannel = member.guild.channels.cache.get('847947064390451272');
-
-    welcChannel.send(`Why do you insist on being here ${member}. Just go the fuck away`);
-
+    member.guild.channels.cache.get(Config.ICIDs.welcome_channel).send(`Welcome ${member.name}!`);
 }
